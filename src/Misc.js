633f30bfd62dd07be20bd6a25fb30b3a9c5d34a2
Justin Samuels <blizm87@gmail.com>
2017-03-17 00:58:27 -0700

    testing trivia game functionality

--- src/Misc.js
+++ src/Misc.js
@@ -6,36 +6,50 @@ class Misc extends Component {
     super();
     this.state = {
       catDefault: '9',
+      trivQuestion: '',
+      trivAnswer: ''
     }
-    this.renderTrivia = this.renderTrivia.bind(this);
+    this.renderTriviaGame = this.renderTriviaGame.bind(this);
     this.renderTriviaControls = this.renderTriviaControls.bind(this);
     this.hidePlayTrivia = this.hidePlayTrivia.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
-  renderTrivia(category){
+  renderTriviaGame(category) {
     //   const data = JSON.parse(body).results[0];
     //   var q = entities.decode(data.question);
-    const url = `https://www.opentdb.com/api.php?amount=1&category=${category}&difficulty=medium&type=multiple`
-    fetch(url).then( res => res.json() ).then( data => {
-      console.log(data)
-    });
+    document.getElementById('preGameNotice').className = 'fadeOut'
+    const url = `https://hidden-reaches-26134.herokuapp.com/?trivQuery=https://www.opentdb.com/api.php?amount=1&category=${category}&difficulty=medium&type=multiple`
+    this.timeout = setTimeout( () => {
+      document.getElementById('preGameNotice').className = 'hiddenControlNone'
+      document.getElementById('trivQA').className = 'hiddenControl'
+      fetch(url).then( res => res.json() ).then( data => {
+        console.log(data)
+        this.setState({
+          trivQuestion: data.data.question,
+          trivAnswer: data.data.correct_answer
+        })
+        document.getElementById('trivQA').className = 'fadeIn'
+      });
+      this.timeout = null
+    }, 1000)
+
   }
 
   renderTriviaControls(){
     this.timeout = setTimeout( () => {
       document.getElementById('catSelect').className = 'pure-button fadeIn'
       document.getElementById('startBtn').className = 'pure-button fadeIn'
-      this.renderTrivia;
+      document.getElementById('triviaGame').className = 'fadeIn'
       this.timeout = null
     }, 700)
-
   }
 
   hidePlayTrivia(evt){
     evt.target.className = 'pure-button fadeOut'
     this.timeout = setTimeout( () => {
+      document.getElementById('triviaGame').className = 'hiddenControl'
       document.getElementById('catSelect').className = 'pure-button hiddenControl'
       document.getElementById('startBtn').className = 'pure-button hiddenControl'
       this.renderTriviaControls();
@@ -48,12 +62,12 @@ class Misc extends Component {
       catDefault: evt.target.value,
     })
     const category = evt.target.value
-    this.renderTrivia(category)
+    this.renderTriviaGame(category)
   }
 
   handleClick(){
     const category = this.state.catDefault
-    this.renderTrivia(category)
+    this.renderTriviaGame(category)
   }
 
   componentWillUnmount() {
@@ -65,6 +79,18 @@ class Misc extends Component {
   render() {
     return (
       <div id="misc">
+        <div className="hiddenControlNone" id="triviaGame">
+          <div>
+            <h3>Trivia Game</h3>
+          </div>
+          <div id="triviaGameInner">
+            <p id="preGameNotice">Press Start when ready</p>
+            <div id="trivQA" className="hiddenControlNone">
+              <p>{this.state.trivQuestion}</p>
+              <p>{this.state.trivAnswer}</p>
+            </div>
+          </div>
+        </div>
         <button className="pure-button hiddenControlNone" id="startBtn" onClick={this.handleClick}>Start Game</button>
         <select className="pure-button hiddenControlNone" onChange={this.handleChange} id="catSelect">
           <option value="9">Random</option>
